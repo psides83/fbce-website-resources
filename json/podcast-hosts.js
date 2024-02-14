5cb6e70d48a7950cae58fa0cbd57256e9568439c
psides83 <60854328+psides83@users.noreply.github.com>
2024-02-13 22:51:07 -0600

    Create podcast-hosts.js

---
+++ json/podcast-hosts.js
@@ -0,0 +1,7 @@
+[
+  {
+    platform: "Apple Podcasts",
+    url: "https://podcasts.apple.com/us/podcast/the-inquiry-room/id1729284644",
+    imgUrl: "https://d1f8ha51vzawnk.cloudfront.net/images/podcast/logo-square/006/apple_podcasts.png"
+  }
+]
